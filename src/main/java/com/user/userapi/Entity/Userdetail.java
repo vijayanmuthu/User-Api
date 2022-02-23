e0def4a15e607504b246e5eeadb5a615fa98d676
vijayanmuthu <vijayanmuthu768@gmail.com>
2022-02-23 16:36:23 +0530

    complete all api and junit testcase

--- src/main/java/com/user/userapi/Entity/Userdetail.java
+++ src/main/java/com/user/userapi/Entity/Userdetail.java
@@ -6,13 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Userdetail {
 
--- src/test/java/com/user/userapi/UserApiApplicationTests.java
+++ src/test/java/com/user/userapi/UserApiApplicationTests.java
@@ -1,15 +1,22 @@
 package com.user.userapi;
 
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Base64;
+
+import javax.transaction.Transactional;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,66 +27,78 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import com.user.userapi.Entity.Userdetail;
+import com.user.userapi.config.JwtRequest;
 import com.user.userapi.repo.UserdetailRepo;
 import com.user.userapi.responce.ResponseVO;
 import com.user.userapi.service.UserService;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
-//@WebMvcTest(UserController.class)
 @AutoConfigureMockMvc
 public class UserApiApplicationTests {
-	
+
 	@Autowired
-    MockMvc mockMvc;
-	
-    @Autowired
-    ObjectMapper mapper;
-    
-    @MockBean
-    UserService userService;
-    
-    @MockBean
-    UserdetailRepo userdetailRepo;
-    
-    @Autowired
-    WebApplicationContext webApplicationContext;
-    
-    @Before
-    public void setup() {
-    	
-    	mockMvc=MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
-    
+	MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper mapper;
+
+	@InjectMocks
+	UserService userService;
+
+	@Mock
+	UserdetailRepo userdetailRepo;
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() {
+
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
 	@Test
+	@Transactional
 	public void testNewUser() throws Exception {
-		
-		Userdetail userdetail =new Userdetail();
+
+		Userdetail userdetail = new Userdetail();
+		userdetail.setId(1);
 		userdetail.setAvatar("test");
 		userdetail.setEmail("test");
 		userdetail.setFirst_name("test");
-		userdetail.setId(1);
 		userdetail.setLast_name("test");
 		userdetail.setPassword("test");
-		Mockito.when(userdetailRepo.save(userdetail)).thenReturn(userdetail);
-		Gson gson =new Gson();
-		String json =gson.toJson(userdetail);
-		MvcResult resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/user")
-				.contentType(MediaType.APPLICATION_JSON)
-	            .accept(MediaType.APPLICATION_JSON)
-	            .content(json)).andReturn();
-		ResponseVO userdetail2 = mapper.readValue(resultActions.getResponse().getContentAsString(),ResponseVO.class);
-		JsonNode json1 =mapper.readTree(mapper.writeValueAsString(userdetail2).toString());
-//		String str =json1.get("email").asText();
-//		System.out.println(str);
+		MvcResult resultActions = mockMvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(userdetail)))
+				.andExpect(status().isOk()).andReturn();
+		System.out.println(resultActions.getResponse().getContentAsString());
 		assertNotNull(resultActions.getResponse().getContentAsString());
-		
+
 	}
-	
+
 	@Test
-	public void testGetUser() {
-		
+	public void testGetUser() throws Exception {
+
+		JwtRequest jwtRequest = new JwtRequest();
+		jwtRequest.setEmail("emma.wong@reqres.in");
+		jwtRequest.setPassword("password");
+		MvcResult resultActions = mockMvc
+				.perform(MockMvcRequestBuilders.post("/authenticate").contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(jwtRequest)))
+				.andReturn();
+
+		ResponseVO userdetail2 = mapper.readValue(resultActions.getResponse().getContentAsString(), ResponseVO.class);
+		JsonNode json1 = mapper.readTree(mapper.writeValueAsString(userdetail2.getResponse()));
+		String token = json1.get("jwttoken").asText();
+		String chunks = token.split("\\.")[1];
+		Base64.Decoder decoder = Base64.getDecoder();
+		String payload = new String(decoder.decode(chunks));
+		JsonNode json = mapper.readTree(payload);
+		String name = json.get("sub").asText();
+		mockMvc.perform(get("/user?email=" + name).header("Authorization", "Bearer " + token))
+				.andExpect(status().isOk());
+
 	}
 }
